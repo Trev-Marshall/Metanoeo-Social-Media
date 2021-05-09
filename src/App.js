864b77ffc18a785234e02a9e447713b63ac51999
Trevor Marshall <trevorhmarshall99@gmail.com>
2021-05-09 08:55:25 -0400

    Added router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import './App.css';
 import styled from 'styled-components';
-import HomeContent from './Components/HomeContent'
+import HomeContent from './Components/HomeContent';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+} from 'react-router-dom';
+import User from './Components/User';
 
 function App() {
   return (
-    <Container>
-      <Navigation>
-        <LogoContainer>
-          <Logo src="https://imgur.com/WGXVyuA.png" />
-        </LogoContainer>
-        <SearchBar placeholder="Search..." />
-        <OptionsDiv>
-          <Notifs>
-            Notifs
-          </Notifs>
-          <UserPhoto src="https://imgur.com/oWr9MTw.png" />
-        </OptionsDiv>
-      </Navigation>
+    <Router>
+      <Container>
+        <Navigation>
+          <LogoContainer>
+            <Link to="/">
+              <Logo src="https://imgur.com/WGXVyuA.png" />
+            </Link>
+          </LogoContainer>
+          <SearchBar placeholder="Search..." />
+          <OptionsDiv>
+            <Notifs>
+              Notifs
+            </Notifs>
+            <Link to="/user">
+              <UserPhoto src="https://imgur.com/oWr9MTw.png" />
+            </Link>
+          </OptionsDiv>
+        </Navigation>
+      </Container>
 
-      <HomeContent />
-    </Container>
+      <Switch>
+        <Route path="/user">
+          <User />
+        </Route>
+        <Route path="/">
+          <HomeContent />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/User.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function User() {
+  return (
+    <Container>
+      User Page
+    </Container>
+  )
+}
+
+export default User
+
+const Container = styled.div`
+  padding-top: 60px; 
+`
