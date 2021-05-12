d7284232c014f2ecde5e57b5337adfe9f73ce10f
Trevor Marshall <trevorhmarshall99@gmail.com>
2021-05-12 13:48:29 -0400

    Added search feature for profiles

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,5 +1,17 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { InstantSearch } from 'react-instantsearch-core';
+import { SearchBox, Hits } from 'react-instantsearch-dom';
+import { useHistory } from 'react-router-dom';
+
+import algoliasearch from 'algoliasearch/lite';
+
+const client = algoliasearch('34SNGEK7ZA', '811b1cb914c6dff45f876c5381af8cbf');
+const index = client.initIndex('metanoeo_profiles');
+
+const searchClient = algoliasearch(
+  '34SNGEK7ZA',
+  '811b1cb914c6dff45f876c5381af8cbf'
+);
 
 function SearchBar() {
   const history = useHistory();
@@ -10,17 +22,26 @@ function SearchBar() {
   }
 
   return (
-    <form onSubmit={onSearch}>
-      <label htmlFor="header-search">
-        <span className="visually-hidden">Search Bar</span>
-      </label>
-      <input
-        type="text"
-        id="header-search"
-        placeholder="Search..."
-        name="s"
-      />
-    </form>
+    <>
+      <InstantSearch
+        indexName="metanoeo_profiles"
+        searchClient={searchClient}
+      >
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
+      <form onSubmit={onSearch}>
+        <label htmlFor="header-search">
+          <span className="visually-hidden">Search Bar</span>
+        </label>
+        <input
+          type="text"
+          id="header-search"
+          placeholder="Search..."
+          name="s"
+        />
+      </form>
+    </>
   )
 }
 
